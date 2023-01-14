667e35be2f15c9ed931edaf8019cce52588febc5
Angalii <122328673+Angalii@users.noreply.github.com>
2023-01-13 19:18:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+
+
+open.addEventListener('click', () => {
+  modal_container.classList.add('show');  
+});
+
+
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
